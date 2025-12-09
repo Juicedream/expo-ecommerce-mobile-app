b9669aad62b88be255b8573f1e3caca942df20d5
Juicedream <judexfrayo@gmail.com>
2025-12-09 13:52:14 +0100

    start server method added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,7 @@ export const connectDB = async () => {
        const conn = await mongoose.connect(ENV.DB_URL);
        console.log(`✅ Connected to MONGODB: ${conn.connection.host}`);
     } catch (error) {
-        console.error("💥MONGODB connection error");
+        console.error("💥MONGODB connection error: ", error.message);
         process.exit(1);
     }
 }
